b70f442701e0fbcd14070be7771cab2afe12123b
kale-io <cos+a@iuvo.ai>
2019-05-05 20:52:56 +0000

    ES6 Property Shorthand Shorter syntax for common object property definition idiom.

--- src/addon-loader.js
+++ src/addon-loader.js
@@ -54,7 +54,7 @@ async function loadAddon(addonPath, verbose) {
   }
   const packageName = manifest.name;
 
-  const pluginClient = new PluginClient(packageName, {verbose: verbose});
+  const pluginClient = new PluginClient(packageName, {verbose});
 
   let addonManagerProxy;
   const pluginClientPromise = pluginClient.register().then((proxy) => {
