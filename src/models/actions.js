f15d6b1a408bca1b4faa89d82375f7b7530435b9
kale-io <cos+a@iuvo.ai>
2019-05-14 19:26:20 +0000

    Revert: Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- src/models/actions.js
+++ src/models/actions.js
@@ -87,7 +87,7 @@ class Actions extends EventEmitter {
 
       return true;
     }).map((action) => {
-      return {action.name: action.getDescription()};
+      return {[action.name]: action.getDescription()};
     });
   }
 
@@ -105,7 +105,7 @@ class Actions extends EventEmitter {
 
       return true;
     }).map((action) => {
-      return {action.name: action.getDescription()};
+      return {[action.name]: action.getDescription()};
     });
   }
 
@@ -139,9 +139,9 @@ class Actions extends EventEmitter {
 
     switch (action.name) {
       case 'pair':
-        AddonManager.addNewThing(action.input.timeout).then(() => {
+        AddonManager.addNewThing(action.input.timeout).then(function() {
           action.updateStatus('completed');
-        }).catch(error => {
+        }).catch(function(error) {
           action.error = error;
           action.updateStatus('error');
           console.error('Thing was not added');
@@ -151,11 +151,11 @@ class Actions extends EventEmitter {
       case 'unpair':
         if (action.input.id) {
           AddonManager.removeThing(action.input.id)
-            .then(thingIdUnpaired => {
+            .then(function(thingIdUnpaired) {
               console.log('unpair: thing:', thingIdUnpaired, 'was unpaired');
               Things.removeThing(thingIdUnpaired);
               action.updateStatus('completed');
-            }).catch(error => {
+            }).catch(function(error) {
               action.error = error;
               action.updateStatus('error');
               console.error('unpair of thing:',
@@ -172,7 +172,7 @@ class Actions extends EventEmitter {
       default:
         delete this.actions[id];
         return Promise.reject(
-          new Error(`Invalid action name: "${action.name}"`));;
+          new Error(`Invalid action name: "${action.name}"`));
     }
     return Promise.resolve();
   }
