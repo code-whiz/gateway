964d4995a5613327c6cdbed140a8a417423cddd9
Iuvo AI <cos+a@iuvo.ai>
2019-09-17 18:22:53 +0000

    Convert Non-strict to strict equality checking Convert non-strict equality checking, using `==`, to the strict version, using `===`.
    tags: changes-semantics

--- src/plugin/plugin.js
+++ src/plugin/plugin.js
@@ -270,7 +270,7 @@ class Plugin {
 
       case Constants.ADAPTER_UNLOADED:
         this.adapters.delete(adapterId);
-        if (this.adapters.size == 0) {
+        if (this.adapters.size === 0) {
           // We've unloaded the last adapter for the plugin, now unload
           // the plugin.
 
@@ -411,7 +411,7 @@ class Plugin {
 
       default:
         console.error('Plugin: unrecognized msg:', msg);
-        break;
+        break;;
     }
   }
 
@@ -456,7 +456,7 @@ class Plugin {
           break;
         }
         default:
-          break;
+          break;;
       }
     }
 
@@ -547,7 +547,7 @@ class Plugin {
 
     this.process.p.on('exit', (code) => {
       if (this.restart) {
-        if (code == Constants.DONT_RESTART_EXIT_CODE) {
+        if (code === Constants.DONT_RESTART_EXIT_CODE) {
           console.log('Plugin:', this.pluginId, 'died, code =', code,
                       'NOT restarting...');
           this.restart = false;
