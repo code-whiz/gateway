b3d2b4b7fa5c3e803355c056ccfc3e5b5eb8d28b
Iuvo AI <cos+a@iuvo.ai>
2019-09-20 02:57:10 +0000

    Revert: Convert function expression to an arrow function Converts a function expression to the new arrow function syntax. Performs the necessary safety checks.

--- src/test/schema-form/test-utils.js
+++ src/test/schema-form/test-utils.js
@@ -3,15 +3,15 @@ const STATIC_JS_PATH = '../../../static/js';
 
 const SchemaForm = require(`${STATIC_JS_PATH}/schema-form/schema-form`);
 
-module.exports.createSchemaForm = ({schema, formData, onSubmit}) => {
+module.exports.createSchemaForm = function({schema, formData, onSubmit}) {
   const schemaForm = new SchemaForm(schema, 'test', 'test', formData, onSubmit);
   const node = schemaForm.render();
   return {schemaForm, node};
 };
-module.exports.fireEvent = (element, event) => {
+module.exports.fireEvent = function(element, event) {
   const evt = document.createEvent('HTMLEvents');
   evt.initEvent(event, true, true);
   return !element.dispatchEvent(evt);
 };
 
-module.exports.makeUndefined = () => {};
+module.exports.makeUndefined = function() {};
